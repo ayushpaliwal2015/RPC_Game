de8cf57d52a76618c6cf9d3b60039f3be86064ec
AYUSH PALIWAL <ayushpaliwal2015@gmail.com>
2022-10-06 01:35:34 -0400

    add main file

---
+++ main.py
@@ -0,0 +1,9 @@
+## Create Virtaul Env ## python -m venv venv
+## Activate Virtual Env ## venv\Scripts\activate
+## Upgrade pip ## python -m venv venv
+## Act
+
+import tkinter as tk
+
+root = tk.Tk()
+root.mainloop()
